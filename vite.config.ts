4811ae8cb04367afc2d088a3ce76cd12b623c118
xzz2021 <zipeng.2016@gmail.com>
2024-01-17 14:14:50 +0800

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -129,7 +129,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
         '/api': {
           target: 'http://127.0.0.1:3000',
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
+          // rewrite: path => path.replace(/^\/api/, '')
         }
       },
       hmr: {
